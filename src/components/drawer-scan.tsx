7e10d4b2c36a05766814f071c196b979777a3f0a
kevinbre <b.kevin@hotmail.es>
2025-01-23 04:12:54 -0300

    fix(test): check if video render inside of drawer

--- src/components/drawer-scan.tsx
+++ src/components/drawer-scan.tsx
@@ -1,8 +1,19 @@
 import {forwardRef, useEffect} from "react";
 
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "./ui/drawer";
+
 interface Props {
     scannedData: any;
-    trigger?: React.ReactNode;
+    trigger: React.ReactNode;
     setPaused: (paused: boolean) => void;
 }
 
@@ -15,17 +26,27 @@ export const DrawerScan = forwardRef<HTMLVideoElement, Props>(({scannedData, set
         };
     }, [setPaused]);
 
-    console.log("DrawerScan render", trigger);
-
     return (
-        <div
-            className={`max-w-96 max-h-60 overflow-hidden flex items-center justify-center relative border-2 ${
-                scannedData ? "border-green-500" : ""
-            }`}
-        >
-            <video ref={ref} />
-            <div className="absolute h-1 w-full bg-red-500 top-0 botom-0 animate-line" />
-        </div>
+        <Drawer>
+            <DrawerTrigger>{trigger}</DrawerTrigger>{" "}
+            <div
+                className={`max-w-96 max-h-60 overflow-hidden flex items-center justify-center relative border-2 ${
+                    scannedData ? "border-green-500" : ""
+                }`}
+            >
+                <video ref={ref} />
+                <div className="absolute h-1 w-full bg-red-500 top-0 botom-0 animate-line" />
+            </div>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Acerque un código de barras</DrawerTitle>
+                    <DrawerDescription />
+                </DrawerHeader>
+                <DrawerFooter>
+                    <DrawerClose />
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
     );
 });
 
